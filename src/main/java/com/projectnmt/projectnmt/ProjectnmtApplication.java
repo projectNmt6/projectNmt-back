3b584a03391f7d86c9ef157d64761ad78e72237d
paroom6 <opwqer3729@gmail.com>
2024-04-02 10:48:29 +0900

    first commit

---
+++ src/main/java/com/projectnmt/projectnmt/ProjectnmtApplication.java
@@ -0,0 +1,13 @@
+package com.projectnmt.projectnmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectnmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectnmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectnmt/projectnmt/ProjectnmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectnmt.projectnmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectnmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
