e582138ce6400174edd09aae5a6dfbe933feef5d
Byeonjichan <jaekyeon14@naver.com>
2024-04-09 15:29:40 +0900

    feat: 회원정보수정

--- src/main/java/com/projectnmt/projectnmt/controller/AccountController.java
+++ src/main/java/com/projectnmt/projectnmt/controller/AccountController.java
@@ -1,20 +1,34 @@
 package com.projectnmt.projectnmt.controller;
 
+import com.projectnmt.projectnmt.dto.EditAccountReqDto;
 import com.projectnmt.projectnmt.dto.RegisterTeamReqDto;
+import com.projectnmt.projectnmt.service.AccountService;
 import com.projectnmt.projectnmt.service.PrincipalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/account")
 public class AccountController {
     @Autowired
     PrincipalService getPrincipalService;
+    @Autowired
+    AccountService accountService;
+
     @GetMapping("/principal")
     public ResponseEntity<?> getPrincipal() {
         return ResponseEntity.ok(getPrincipalService.getPrincipal());
     }
 
+    @PutMapping("/mypage/edit")
+    public ResponseEntity<?> accountEdit(@RequestBody EditAccountReqDto editAccountReqDto,
+                                         BindingResult bindingResult) {
+        System.out.println(editAccountReqDto);
+            accountService.editAccount(editAccountReqDto);
+            return ResponseEntity.ok().build();
+    }
 }
-
--- src/main/java/com/projectnmt/projectnmt/repository/UserMapper.java
+++ src/main/java/com/projectnmt/projectnmt/repository/UserMapper.java
@@ -4,8 +4,9 @@ import com.projectnmt.projectnmt.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface UserMapper {
+public interface  UserMapper {
     public User findUserTest();
     public User findUserByUsername(String username);
     public int saveUser(User user);
+    public int updateUser(User user);
 }
