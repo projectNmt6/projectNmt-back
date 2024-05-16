09dbe9c320045cc3b80bd411511f0c7c10bf251b
paroom6 <148047669+paroom6@users.noreply.github.com>
2024-05-16 09:45:03 +0900

    89   join team (#95)
    * feat: 배포용
    
    * 시큐리티 변경

--- src/main/java/com/projectnmt/projectnmt/config/SecurityConfig.java
+++ src/main/java/com/projectnmt/projectnmt/config/SecurityConfig.java
@@ -37,7 +37,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors();//WebMvcConfig의 cors 설정을 따라간다.
         http.csrf().disable();
         http.authorizeRequests()
+<<<<<<< HEAD
+                .antMatchers("/main/**","/tag/**", "/auth/**", "/comment/**", "/challenge/**", "/like/get", "/donator", "/team/info", "/env")
+=======
                 .antMatchers("/main/**","/team/**", "/auth/**", "/comment/**", "/challenge/**", "/main/challenges", "/challengeComment/get/**", "/like/get", "/donator" )
+>>>>>>> refs/remotes/origin/89---joinTeam
                 .permitAll()
                 .antMatchers("/team/info")
                 .permitAll()
